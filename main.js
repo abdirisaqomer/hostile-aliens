299d32199486ebd652efed4d532abac374c590ef
Abdirisaq Omer <Abdirisaq.Omer@Nology.local>
2020-02-27 12:29:14 +0000

    First Commit

---
+++ main.js
@@ -0,0 +1,56 @@
+// Ship blueprint
+class Ship {
+    constructor(health) {
+        // this.name = name;
+        this.health = health;
+    }
+}
+
+//Mothership Class
+class MotherShip extends Ship {
+    constructor() {
+        super(100);
+    }
+
+    decrementHealth() {
+        this.health = this.health - 9;
+    }
+}
+
+//Attack Ships
+class AttackShip extends Ship {
+    constructor() {
+        // this adds 45 to the health in 
+        super(45);
+    }
+
+    decrementHealth() {
+        this.health = this.health - 12;
+    } 
+
+}
+
+const motherShip = new MotherShip();
+const attackShip1 = new AttackShip();
+const attackShip2 = new AttackShip();
+const attackShip3 = new AttackShip();
+const attackShip4 = new AttackShip();
+const attackShip5 = new AttackShip();
+
+
+motherShip.decrementHealth();
+attackShip1.decrementHealth();
+attackShip2.decrementHealth();
+attackShip3.decrementHealth();
+attackShip4.decrementHealth();
+attackShip5.decrementHealth();
+
+console.log("Mother Ship ",  motherShip);
+console.log("Attack Ship 1 ", attackShip1);
+console.log("Attack Ship 2 ", attackShip2);
+console.log("Attack Ship 3 ", attackShip3);
+console.log("Attack Ship 4 ", attackShip4);
+console.log("Attack Ship 5 ", attackShip5);
+
+
+
